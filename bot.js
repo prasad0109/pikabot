844f140427cfe5813ecaaff5af34dcc4a5860468
prasad0109 <drivezoneyt09@gmail.com>
2026-05-23 23:17:44 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ rl.on('line', (input) => {
 
 // ===== CREATE BOT =====
 function createBot() {
-  const name = process.argv[2] || "ZzZebra";
+  const name = process.argv[2] || "PRASAD09";
   console.log(`Creating bot... [${name}]`);
 
   let server = settings.server;
