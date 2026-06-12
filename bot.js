2b58695b0859f4e47f2dbe11c1c675dad0cb7e22
prasad0109 <drivezoneyt09@gmail.com>
2026-06-12 15:31:35 +0530

    Refactor bot.js to improve structure and readability

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ function createBot() {
       setTimeout(() => {
         if (!bot) return;
 
-        bot.chat("/login PRASAD@2009");
+        bot.chat("/login prasadpradeep2009");
 
         setTimeout(() => {
           if (!bot) return;
